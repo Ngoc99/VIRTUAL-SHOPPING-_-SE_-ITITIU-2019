e1334f8cfa4a137dff35c4a594bddd07f45a6576
dpq3005 <41153934+dpq3005@users.noreply.github.com>
2019-12-01 18:33:01 +0700

    Login_Logout_Register

--- CODING_SESSION/index.js
+++ CODING_SESSION/index.js
@@ -1,12 +1,38 @@
 let express = require('express');
 let app = express();
+let mongoose = require('mongoose');
+let bodyParser = require('body-parser');
+let expressSession = require('express-session');
+
+let { USER_ROUTE } = require('./route/user')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressSession({
+  secret: 'DPQ',
+  saveUninitialized: true,
+  resave: true,
+  cookie: {
+    maxAge: 10 * 60 * 1000 // milli
+  }
+}))
 
 app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use('/', USER_ROUTE);
 
 app.get('/', (req, res) => {
   res.json({ message: 'Success' });
 });
 
-app.listen(3000, () => {
-  console.log(`Server started at port 3000`);
+let uri = `mongodb://localhost/Test`;
+
+mongoose.connect(uri);
+mongoose.connection.once('open', () => {
+  console.log('mongodb connected');
+  app.listen(3000, () => {
+    console.log(`Server started at port 3000`);
+  });
+
 });
